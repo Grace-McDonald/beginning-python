f5bcd2e465012d5a1b3f924d3d6ecd22722fb9c1
Grace McDonald <gmcdonald@stu.kerikerihigh.ac.nz>
2022-03-20 21:52:31 +1300

    heeh

---
+++ Q9.1.py
@@ -0,0 +1,26 @@
+number = int(input("Please choose a number: "))
+times1 = number * 1
+print(f"1 times {number} is {times1} ")
+times2 = number * 2
+print(f"2 times {number} is {times2} ")
+times3 = number * 3
+print(f"3 times {number} is {times3} ")
+times4 = number * 4
+print(f"4 times {number} is {times4} ")
+times5 = number * 5
+print(f"5 times {number} is {times5} ")
+times6 = number * 6
+print(f"6 times {number} is {times6} ")
+times7 = number * 7
+print(f"7 times {number} is {times7} ")
+times8 = number * 8
+print(f"8 times {number} is {times8} ")
+times9 = number * 9
+print(f"9 times {number} is {times9} ")
+times10 = number * 10
+print(f"10 times {number} is {times10} ")
+times11 = number * 11
+print(f"11 times {number} is {times11} ")
+times12 = number * 12
+print(f"12 times {number} is {times12} ")
+# I know there is a faster way to do this but I was in a rush and couldn't be bothered to do it
